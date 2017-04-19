a7d3a14b2f19fbc752fc56a3b21bd2f905870583
Virtucio <VirtucioC@pragmatics.com>
2017-04-18 22:12:44 -0400

    feat(Info): interface
    - added more methods to Info interface

--- src/main/java/funWithRx/service/InfoFactory.java
+++ src/main/java/funWithRx/service/InfoFactory.java
@@ -4,6 +4,6 @@ package funWithRx.service;
  * Created by cvirtucio on 4/18/2017.
  */
 public class InfoFactory {
-    public Info createFileInfo(String fileID) {
+    public Info createFileInfo(String fileName, Long uploadLength, Long fileOffset) {
     };
 }
