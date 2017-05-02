edad386f30ff177be8dc9be0f0ea6bdb1680a76c
cjvirtucio87 <cjv287@gmail.com>
2017-05-02 07:40:47 -0400

    refactor(Dashboard): handlers
    - added handlers for initial HEAD request for checking if the file exists and the preliminary POST request for creating a directory

--- src/client/src/container/Dashboard/index.tsx
+++ src/client/src/container/Dashboard/index.tsx
@@ -8,7 +8,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { PART_SIZE } from '../../constants';
+import { PART_SIZE, BASE_URI, FILENAME_PATTERN } from '../../constants';
 import { addFile, updateProgress } from '../../actions';
 
 import presentational from '../../presentational/';
@@ -31,6 +31,14 @@ const capAtFilesize = (value, fileSize) => value > fileSize ? fileSize : value;
 
 const createFileParts = (file, fileName, uploadOffset, uploadLength, partNumber, parts) => {
   if (uploadOffset >= file.size) return parts;
+
+  // 0:2, 3:4, 5:6
+  // 0:2, 0:1, 0:1
+  // 2 bytes to be transferred; 1 byte to be transferred; 1 byte to be transferred;
+  // bytesToBeTransferred = (len - offset)
+  // upperBoundPart = bytesToBeTransferred
+  // lowerBoundPart = 0
+  // Therefore, we only transfer bytes when (bytesToBeTransferred - lowerBoundPart) > 0;
   
   parts.push({
     file: file.slice(uploadOffset, uploadLength + 1),
@@ -39,13 +47,35 @@ const createFileParts = (file, fileName, uploadOffset, uploadLength, partNumber,
     uploadOffset: capAtFilesize(uploadOffset, file.size),
     uploadLength: capAtFilesize(uploadLength, file.size)
   });
-  return createFileParts(file, fileName, uploadOffset + PART_SIZE , uploadLength + PART_SIZE, partNumber + 1, parts);
+  return createFileParts(file, fileName, capAtFilesize(uploadOffset + PART_SIZE, file.size), capAtFilesize(uploadLength + PART_SIZE, file.size), partNumber + 1, parts);
+}
+
+const onFileNotExist = (dispatch, fileName, parts) => () => {
+  console.log(`File not found. Creating directory for file, ${fileName}`);
+  axios.post(`${BASE_URI}`, null, {
+    headers: {
+      fileName
+    }
+  }).then(resp => {
+    const { headers } = resp;
+    console.log(`Created directory, ${headers.filedir}`);
+    dispatch(addFile(parts));
+  });
 }
 
 const onLoadEnd = (dispatch, file) => () => {
-  const fileName = /^(.+)\..*/.exec(file.name)[1];
+  const fileName = FILENAME_PATTERN.exec(file.name)[1];
   const parts = createFileParts(file, fileName, 0, PART_SIZE, 0, []);
-  dispatch(addFile(parts));
+  const partNumbers = parts.map(part => part.partNumber);  
+
+  axios.head(`${BASE_URI}`, {
+    headers: {
+      fileName,
+      partNumbers
+    }
+  })
+  .then(resp => console.log(resp))
+  .catch(onFileNotExist(dispatch, fileName, parts));
 }
 
 const onAddFile = dispatch => event => {
@@ -56,13 +86,14 @@ const onAddFile = dispatch => event => {
 }
 
 const uploadPart = dispatch => startTime => part => {
-  const { partNumber, uploadLength, file, fileName } = part;
+  const { partNumber, uploadOffset, uploadLength, file, fileName } = part;
 
-  return axios.patch(`http://localhost:8080/upload/${fileName}`, file, {
+  return axios.patch(`${BASE_URI}/${fileName}`, file, {
     headers: {
       'content-type': 'text/plain',
       fileName,
       partNumber,
+      uploadOffset,
       uploadLength,
       userName: 'cjvirtucio'
     },
@@ -73,7 +104,8 @@ const uploadPart = dispatch => startTime => part => {
       dispatch(updateProgress({ partNumber, progress, speed }));
       // if (progress === 100) dispatch(donePart(file));
     }
-  });
+  })
+  .then(resp => console.log(resp));
 };
 
 const onUploadFile = dispatch => parts => event => {
