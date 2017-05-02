595c22e6f695daa2e17eaa46f9ebb50075821b7e
cjvirtucio87 <cjv287@gmail.com>
2017-05-02 07:48:40 -0400

    refactor(UploadUtil): methods
    - added static method for getting a file pointer
    - added static method for mapping a list of part numbers to a list of file pointers
    - added static method for creating a directory
    - moved some variables in the writeFilePart method
    - changed logic for checkIfComplete method
    - changed file path generated by createFilePath method to include directory

--- src/main/java/ReactMember/service/UploadUtil.java
+++ src/main/java/ReactMember/service/UploadUtil.java
@@ -8,7 +8,15 @@ import java.io.RandomAccessFile;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.io.File.separator;
 
 /**
  * Created by cvirtucio on 4/20/2017.
@@ -17,53 +25,126 @@ import java.util.function.Function;
 public class UploadUtil {
     private static final Logger log = Logger.getLogger(UploadUtil.class);
 
-    public static Long writeFilePart(PartInfo partInfo) {
+    public static Long getFilePointer(PartInfo partInfo) {
+        String filePath = createFilePath(partInfo);
+        Long filePointer = 0L;
+        RandomAccessFile raf = null;
+
         try {
-            String filePath = createFilePath(partInfo);
-            log.info("Opening channels for file, " + filePath);
-            RandomAccessFile raf = new RandomAccessFile(filePath, "rw");
-            ReadableByteChannel is = UploadUtil.getByteChannel(raf.getFilePointer(), partInfo);
-            FileChannel os = raf.getChannel();
-            Long newOffset = 0L;
-
-            try {
-                log.info("Writing file, " + filePath);
-                Long bytesToTransfer = partInfo.getUploadLength() - raf.getFilePointer();
-                if (bytesToTransfer > 0) {
-                    os.transferFrom(is, raf.getFilePointer(), bytesToTransfer);
-                }
-            } catch (IOException e) {
-                log.error("Error writing file, " + filePath);
-                log.error("Error", e);
-            } finally {
-                log.info("Closing channels for, " + filePath);
-                newOffset = raf.getFilePointer();
-                is.close();
-                raf.close();
-            }
-
-            return newOffset;
+            raf = new RandomAccessFile(filePath, "rw");
+            filePointer = raf.getFilePointer();
         } catch (Exception e) {
-            log.error("Error, ", e);
+            log.error("Error attempting to get offset for file, " + filePath);
+        } finally {
+            try {
+                raf.close();
+            } catch (Exception e) {
+                log.error("Error closing RandomAccessFile for file, " + filePath);
+            }
         }
 
-        return null;
+        return filePointer;
     }
 
-    public static Function<Long, Boolean> checkIfComplete(PartInfo partInfo) {
-        return filePointer -> filePointer == partInfo.getUploadLength();
+    public static String createDirectory(String fileName) {
+        Path path = Paths.get(System.getProperty("java.io.tmpdir") + separator + fileName);
+        try {
+            Files.createDirectory(path);
+            log.info("Created file directory, " + path.toString());
+        } catch (Exception e) {
+            log.error("Failed to create directory, " + path.toString());
+        } finally {
+            return path.toString();
+        }
+    }
+
+    public static Function<String, List<Long>> getFilePointerList(List<Long> partNumbers) {
+        return fileName -> partNumbers.stream()
+                    .map(num -> {
+                        String filePath = System.getProperty("java.io.tmpdir") + fileName + separator + fileName + "_" + num.toString();
+                        RandomAccessFile raf = null;
+                        Long filePointer = 0L;
+
+                        try {
+                            raf = new RandomAccessFile(filePath, "r");
+                            filePointer = raf.getFilePointer();
+                        } catch (Exception e) {
+                            log.error("Error attempting to read file, " + filePath);
+                        } finally {
+                            try {
+                                raf.close();
+                            } catch (Exception e) {
+                                log.error("Error attempting to close RandomAccessFile for file, " + filePath);
+                            }
+                        }
+
+                        return filePointer;
+                    })
+                    .collect(Collectors.toList());
+    }
+
+    public static Boolean checkIfExists(String fileName) {
+        Path path = Paths.get(System.getProperty("java.io.tmpdir") + separator + fileName);
+        return Files.exists(path);
+    }
+
+    // TODO: writeFilePart stops halfway through; fix it
+    public static Long writeFilePart(PartInfo partInfo) {
+        String filePath = createFilePath(partInfo);
+        Long currentOffset = 0L;
+        Long newOffset = 0L;
+        Long bytesTransferred = 0L;
+        RandomAccessFile raf = null;
+        ReadableByteChannel is = null;
+        FileChannel os;
+
+        try {
+            log.info("Opening channels for file, " + filePath);
+            raf = new RandomAccessFile(filePath, "rw");
+            is = UploadUtil.getByteChannel(currentOffset, partInfo);
+            os = raf.getChannel();
+            currentOffset = raf.getFilePointer();
+
+            log.info("Writing file, " + filePath);
+            Long bytesToTransfer = (partInfo.getUploadLength() - partInfo.getUploadOffset()) - currentOffset;
+            if (bytesToTransfer > 0) {
+                bytesTransferred = os.transferFrom(is, raf.getFilePointer(), bytesToTransfer);
+            }
+
+        } catch (IOException e) {
+            log.error("Error writing file, " + filePath);
+            log.error("Error", e);
+        } finally {
+            log.info("Closing channels for, " + filePath);
+            newOffset = currentOffset + bytesTransferred;
+
+            try {
+                is.close();
+                raf.close();
+            } catch (Exception e) {
+                log.error("Error in attempt to close channels for, " + filePath);
+            } finally {
+                return newOffset;
+            }
+        }
+    }
+
+    public static Predicate<Long> checkIfComplete(PartInfo partInfo) {
+        // TODO: fix this filter
+        return filePointer -> filePointer == (partInfo.getUploadLength() - partInfo.getUploadOffset());
     }
 
     /**
      * The path to the file part consists of the following components:
      * (1) the tmp directory
-     * (2) the file name
-     * (3) the part number
+     * (2) the file name as a folder
+     * (3) the file name on the file itself
+     * (4) the part number
      * @param partInfo
      * @return the file path for the part to be written
      */
     private static String createFilePath(PartInfo partInfo) {
-        String path = System.getProperty("java.io.tmpdir") + partInfo.getFileName() + "_" + partInfo.getPartNumber();
+        String path = System.getProperty("java.io.tmpdir") + separator + partInfo.getFileName()  + separator + partInfo.getFileName() + "_" + partInfo.getPartNumber();
         String message = "Creating file part, " + path;
         log.info(message);
         return path;
