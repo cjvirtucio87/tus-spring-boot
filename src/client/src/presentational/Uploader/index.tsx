ab615a727b9ed35aa6082efd0cc0ecbe52d0ec3f
cjvirtucio87 <cjv287@gmail.com>
2017-05-06 22:25:06 -0400

    feat(Uploader): ChunkToggle
    - added chunked prop
    - added chunkToggle button

--- src/client/src/presentational/Uploader/index.tsx
+++ src/client/src/presentational/Uploader/index.tsx
@@ -3,10 +3,12 @@ import * as update from 'immutability-helper';
 
 import axios from 'axios';
 
-const Uploader = ({ onAddFile, onUploadFile, parts }) => (
+const Uploader = ({ onAddFile, onUploadFile, onChunkToggle, parts, chunked }) => (
   <div className='Uploader'>
       <label htmlFor='fileUploader'>File Uploader</label>
-      <input id='fileUploader' type='file' onChange={ onAddFile } /><br/>
+      <h3>{ chunked ? "CHUNKED MODE" : "SINGLE UPLOAD MODE" }</h3>
+      <input id='fileUploader' type='file' onChange={ onAddFile(chunked) } /><br/>
+      <button type='button' onClick={ onChunkToggle }>Toggle Chunk Upload</button><br/>
       <button type='button' onClick={ onUploadFile(parts) }>Upload</button><br/>
   </div>
 );
