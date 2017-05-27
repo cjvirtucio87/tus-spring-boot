7d1426c7111dd723deb507ca48a699b177f52828
cjvirtucio87 <cjv287@gmail.com>
2017-05-27 16:00:43 -0400

    chore(Package): name
    - changed package name

--- src/main/java/tusspringboot/service/UploadUtil.java
+++ src/main/java/tusspringboot/service/UploadUtil.java
@@ -1,7 +1,6 @@
-package TusSpringBoot.service;
+package tusspringboot.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.nio.channels.Channels;
@@ -11,7 +10,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.ToLongFunction;
+
+import static tusspringboot.Constants.TMP_DIR;
 
 /**
  * Created by cvirtucio on 4/20/2017.
@@ -24,60 +25,45 @@ public class UploadUtil {
         String filePath = createFilePath(partInfo);
         log.info("Retrieving pointer for file part, " + filePath);
         Long currentOffset = 0L;
-        RandomAccessFile raf = null;
 
-        try {
-            raf = new RandomAccessFile(filePath, "rw");
+        try (RandomAccessFile raf = new RandomAccessFile(filePath, "rw");) {
             currentOffset = raf.length();
         } catch (Exception e) {
             log.error("Error attempting to get offset for file, " + filePath);
-        } finally {
-            try {
-                raf.close();
-            } catch (Exception e) {
-                log.error("Error closing RAF channel for file, " + filePath);
-            }
         }
 
         return currentOffset;
     }
 
     public static String createDirectory(String fileName) {
-        Path path = Paths.get(
-                System.getProperty("java.io.tmpdir"),
-                fileName
-        );
+        Path path = Paths.get(TMP_DIR, fileName);
         try {
             Files.createDirectory(path);
             log.info("Created file directory, " + path.toString());
         } catch (Exception e) {
             log.error("Failed to create directory, " + path.toString());
-        } finally {
-            return path.toString();
         }
+
+        return path.toString();
     }
 
     public static Boolean checkIfExists(String fileName) {
-        Path path = Paths.get(System.getProperty("java.io.tmpdir"), fileName);
-        return Files.exists(path);
+        return Paths.get(TMP_DIR, fileName).toFile().exists();
     }
 
     public static PartInfo writeFilePart(PartInfo partInfo) {
         String filePath = createFilePath(partInfo);
-        Long newOffset = 0L;
         Long bytesTransferred = 0L;
-        RandomAccessFile raf = null;
-        ReadableByteChannel is = null;
-        FileChannel os;
 
-        try {
+        try (
+                RandomAccessFile raf = new RandomAccessFile(filePath, "rw");
+                ReadableByteChannel is = Channels.newChannel(partInfo.getInputStream());
+                FileChannel os = raf.getChannel();
+                ) {
             log.info("Opening channels for file part, " + filePath);
-            raf = new RandomAccessFile(filePath, "rw");
-            is = Channels.newChannel(partInfo.getInputStream());
-            os = raf.getChannel();
-
             log.info("Writing file part, " + filePath);
             Long bytesToTransfer = (partInfo.getUploadLength() - partInfo.getUploadOffset());
+
             if (bytesToTransfer > 0) {
                 bytesTransferred = os.transferFrom(is, os.size(), bytesToTransfer);
             }
@@ -85,76 +71,63 @@ public class UploadUtil {
             log.info("Done writing file part, " + filePath);
         } catch (IOException e) {
             log.error("Error writing file part, " + filePath);
-            log.error("Error", e);
-        } finally {
-            log.info("Closing channels for file part, " + filePath);
-            newOffset = bytesTransferred + partInfo.getUploadOffset();
-
-            try {
-                raf.close();
-                is.close();
-            } catch (Exception e) {
-                log.error("Error in attempt to close channels for file part, " + filePath);
-            } finally {
-                return PartInfo.builder()
-                        .uploadOffset(newOffset)
-                        .uploadLength(partInfo.uploadLength)
-                        .fileName(partInfo.fileName)
-                        .partNumber(partInfo.partNumber)
-                        .build();
-            }
         }
+
+        return PartInfo.builder()
+                .uploadOffset(bytesTransferred + partInfo.getUploadOffset())
+                .uploadLength(partInfo.uploadLength)
+                .fileName(partInfo.fileName)
+                .partNumber(partInfo.partNumber)
+                .build();
     }
 
     public static boolean checkIfComplete(PartInfo partInfo) {
         return partInfo.uploadOffset.equals(partInfo.uploadLength);
     }
 
-    public static String concatenate(List<PartInfo> partInfoList) {
+    public static Long concatenate(List<PartInfo> partInfoList) {
         String fileName = partInfoList.get(0).getFileName();
-        Long fileSize = partInfoList.get(0).getFileSize();
-        String finalPath = Paths.get(
-                System.getProperty("java.io.tmpdir"),
-                fileName,
-                fileName
-        ).toString();
-        RandomAccessFile raf = null;
-        FileChannel os = null;
-        Long currentOffset = 0L;
+        String finalPath = Paths.get(TMP_DIR, fileName, fileName).toString();
+        Long totalBytesTransferred = 0L;
 
-        try {
-            log.info("Concatenating file parts for file, " + finalPath);
-            raf = new RandomAccessFile(finalPath, "rw");
-            os = raf.getChannel();
-
-            for (PartInfo partInfo : partInfoList) {
-                Path partPath = Paths.get(
-                        System.getProperty("java.io.tmpdir"),
-                        partInfo.getFileName(),
-                        partInfo.getFileName() + "_" + partInfo.getPartNumber()
-                );
-                InputStream is = Files.newInputStream(partPath);
-                try {
-                    currentOffset += os.transferFrom(Channels.newChannel(is), currentOffset, fileSize);
-                } catch (Exception e) {
-                    log.error("Error attempting to concatenate file parts for file, " + finalPath);
-                }
-            }
-
-            log.info("Done concatenating file parts for file, " + finalPath);
-        } catch (Exception e) {
-            log.error("Error attempting to get output stream for file, " + finalPath);
-        } finally {
-            log.info("Closing channels for file, " + finalPath);
-            try {
-                raf.close();
-                os.close();
-            } catch (Exception e) {
-                log.error("Error closing channels for concatenation of file, " + finalPath);
-            }
+        try (
+                RandomAccessFile raf = new RandomAccessFile(finalPath, "rw");
+                FileChannel outputStream = raf.getChannel();
+                ) {
+            log.info("Concatenating file, " + fileName);
+            totalBytesTransferred = partInfoList.stream()
+                    .mapToLong(UploadUtil.toBytesTransferred(outputStream))
+                    .sum();
+        } catch (IOException e) {
+            log.error("Error attempting to concatenate parts for file, " + fileName);
         }
 
-        return fileName;
+        return totalBytesTransferred;
+    }
+
+    private static ToLongFunction<PartInfo> toBytesTransferred(FileChannel outputStream) {
+        return partInfo -> {
+            Long bytesTransferred = 0L;
+            try {
+                InputStream is = Files.newInputStream(
+                        Paths.get(
+                                TMP_DIR,
+                                partInfo.getFileName(),
+                                partInfo.getFileName() + "_" + partInfo.getPartNumber()
+                        )
+                );
+
+                bytesTransferred = outputStream.transferFrom(
+                        Channels.newChannel(is),
+                        outputStream.size(),
+                        partInfo.getFileSize()
+
+                );
+            } catch (IOException e) {
+                log.error("Error during file concatenation for file, " + partInfo.getFileName());
+            }
+            return bytesTransferred;
+        };
     }
 
     /**
@@ -170,18 +143,11 @@ public class UploadUtil {
      * @return the file path for the part to be written
      */
     private static String createFilePath(PartInfo partInfo) {
-        String path = Paths.get(
+        return Paths.get(
                 System.getProperty("java.io.tmpdir"),
                 partInfo.getFileName(),
                 partInfo.getFileName() + "_" + partInfo.getPartNumber()
         ).toString();
-        return path;
-    }
-
-    private static ReadableByteChannel getByteChannel(Long filePointer, PartInfo partInfo) throws Exception {
-        InputStream is = partInfo.getInputStream();
-        is.skip(filePointer);
-        return Channels.newChannel(is);
     }
 
     private UploadUtil() {
