41b50ae6ad57e8b877dfaec1dfa6fe72893d8821
cjvirtucio87 <cjv287@gmail.com>
2017-05-27 15:58:33 -0400

    chore(Constants): tmp
    - extracted references to tmpdir to a constant

--- src/main/java/tusspringboot/Constants.java
+++ src/main/java/tusspringboot/Constants.java
@@ -1,8 +1,10 @@
-package TusSpringBoot;
+package tusspringboot;
 
 /**
  * Created by cvirtucio on 4/19/2017.
  */
 public class Constants {
+    public static final String TMP_DIR = System.getProperty("java.io.tmpdir");
+
     public static final long PART_SIZE = 1054 * 1054 * 5;
 }
