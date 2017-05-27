77b5afd2d861303535a3cb9a88bd57aa9b138a7f
cjvirtucio87 <cjv287@gmail.com>
2017-05-27 14:27:15 -0400

    refactor(Spring): config
    - changed config to App.java instead of controller

--- src/main/java/TusSpringBoot/App.java
+++ src/main/java/TusSpringBoot/App.java
@@ -9,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(UploadController.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
