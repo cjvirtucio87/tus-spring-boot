72fafd8d24cfb6e11ae3379f7139c32e19ae9e25
cjvirtucio87 <cjv287@gmail.com>
2017-05-29 16:51:45 -0400

    test(UploadFileReader):
    - added unit tests for UploadFileReader

--- src/main/test/tusspringboot/service/UploadFileReaderTest.java
+++ src/main/test/tusspringboot/service/UploadFileReaderTest.java
@@ -1,7 +1,102 @@
 package tusspringboot.service;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static tusspringboot.Constants.*;
+
 /**
  * Created by cjvirtucio on 5/29/17.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class UploadFileReaderTest {
+
+    @InjectMocks
+    UploadFileReader uploadFileReader;
+
+    private PartInfo testPartInfo = PartInfo.builder().fileName(TEST_FILENAME).partNumber(0L).build();
+
+    private Path testDirPath = PathFactory.createDirectoryPath(TEST_FILENAME);
+
+    private Path testPartPath = PathFactory.createPartPath(testPartInfo);
+
+    @Before
+    public void setup() throws IOException {
+        Files.createDirectory(testDirPath);
+        Files.createFile(testPartPath);
+    }
+
+    @Test
+    public void checkIfExists_ReturnTrue_OnDirectoryExists() {
+        assertTrue(uploadFileReader.checkIfExists(TEST_FILENAME));
+    }
+
+    @Test
+    public void checkIfExists_ReturnFalse_OnDirectoryNotExist() {
+        assertFalse(uploadFileReader.checkIfExists(TEST_FILENAME + "HELLO"));
+    }
+
+    @Test
+    public void checkIfComplete_ReturnTrue_OnOffsetMatchesLength() {
+        PartInfo partInfo = PartInfo.builder()
+                .uploadOffset(TEST_UPLOADOFFSET + TEST_UPLOADOFFSET_INC_COMPLETE)
+                .uploadLength(TEST_UPLOADLENGTH)
+                .build();
+
+        assertTrue(uploadFileReader.checkIfComplete(partInfo));
+    }
+
+    @Test
+    public void checkIfComplete_ReturnFalse_OnOffsetLessThanLength() {
+        PartInfo partInfo = PartInfo.builder()
+                .uploadOffset(TEST_UPLOADOFFSET)
+                .uploadLength(TEST_UPLOADLENGTH)
+                .build();
+
+        assertFalse(uploadFileReader.checkIfComplete(partInfo));
+    }
+
+    @Test
+    public void getCurrentOffset_ReturnZero_OnEmptyFile() throws IOException {
+        PartInfo partInfo = PartInfo.builder()
+                .fileName(TEST_FILENAME)
+                .partNumber(0L)
+                .build();
+
+        assertEquals(0L, (long) uploadFileReader.getCurrentOffset(partInfo));
+    }
+
+    @Test
+    public void getCurrentOffset_ReturnLong_OnWrittenFile() throws IOException {
+        PartInfo partInfo = PartInfo.builder()
+                .fileName(TEST_FILENAME)
+                .partNumber(0L)
+                .build();
+
+        byte[] byteArray = new byte[]{ 0, 0, 0 };
+
+        Files.write(testPartPath, byteArray);
+
+        assertEquals(byteArray.length, (long) uploadFileReader.getCurrentOffset(partInfo));
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        testPartPath.toFile().delete();
+        testDirPath.toFile().delete();
+    }
 }
