6ead01cd28b530161703b73389f39f9da3d0fb6d
cjvirtucio87 <cjv287@gmail.com>
2017-07-04 18:35:31 -0400

    feat(Web): index
    - added controller for serving index template

--- src/main/java/com/tusspringboot/controller/IndexController.java
+++ src/main/java/com/tusspringboot/controller/IndexController.java
@@ -1,7 +1,20 @@
-package com.tusspringboot;
+package com.tusspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by cjvirtucio on 7/4/17.
  */
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
 }
