93fece202a2dbf3696eff0aefcd07aae58432867
cvirtucio <cvirtucio@mitre.org>
2018-01-20 20:13:11 -0500

    feat(Init): proxy
    - now passing proxy args

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ which docker > /dev/null
 
 if [ $? = 0 ]; then
   echo "Docker detected. Building image."
-  docker build -t tus-spring-boot .
+  cp $M2_HOME/conf/settings.xml .
+  docker build \
+      --build-arg HTTP_PROXY=$HTTP_PROXY \
+      --build-arg HTTP_PROXY_PORT=$HTTP_PROXY_PORT \
+      -t tus-spring-boot .
   echo "Done."
   echo "Running tus-spring-boot. Cheers!"
+  rm settings.xml
   docker run -p 8080:8080 -t --name=tus-spring-boot tus-spring-boot
 else
   echo "Docker not detected." 
