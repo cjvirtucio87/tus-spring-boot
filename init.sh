2d9ad8e31d1d7a3afa23d8a49c68d72a470dff00
cjvirtucio87 <cjv287@gmail.com>
2018-01-29 21:34:23 -0500

    1/29/18
    - added more features to init script (README TBD)

--- init.sh
+++ init.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 client_dir=src/client
+jar_path=target/tus-spring-boot-1.0-SNAPSHOT.jar
 settings=$M2_HOME/conf/settings.xml
 
 function get_client {
   git clone git@github.com:cjvirtucio87/tus-spring-boot-client.git $client_dir 
+}
+
+function build_client {
+	pushd $client_dir
+	npm install && \
+    npm run build && \
+		popd && \
+		mv $client_dir/build/ src/main/resources/public/
 }
 
 function build_image {
@@ -19,47 +28,93 @@ function run_container {
   docker run -p 8080:8080 -t --name=tus-spring-boot tus-spring-boot
 }
 
-function build_client {
-	pushd $client_dir
-	npm install && \
-    npm run build && \
-		popd && \
-		mv $client_dir/build/ src/main/resources/public/
+function build_app {
+  local opt=$1
+
+  case $opt in
+    "--help" )
+      print_help 
+      exit 0 ;;
+    "--clean-mvn" )
+      mvn clean install ;;
+    "--clean-client" )
+      build_client && mvn install ;;
+    "--clean-all" )
+      build_client && mvn clean install ;;
+    "" )
+      echo "No option passed. Running the application." ;;
+    * )
+      echo "Error! Invalid argument!"
+      exit 1;
+  esac
 }
 
 function run_app {
-  mvn clean install && \
-	  java -jar target/tus-spring-boot-1.0-SNAPSHOT.jar
+	java -jar $jar_path
+}
+
+function print_help {
+  local msg="This is the init script for the TusSpringBoot project."
+  msg="$msg\n[OPTIONS]"
+  msg="$msg\n--help (print this help message)"
+  msg="$msg\n--clean-mvn (run the app with the 'clean' argument for maven)"
+  msg="$msg\n--clean-client (run the app with a fresh production build of the reactjs client)"
+  msg="$msg\n--clean-all (run the app with a fresh build of both the server and the client)"
+  printf "$msg\n"
 }
 
 function main {
+  local opt=$1
+ 
+  echo "Downloading client."
+  get_client
+  if [ $? = 0 ]; then 
+    echo "Done."
+  else
+    echo "Error! Failed to download client!"
+    exit 1
+  fi
+
   which docker > /dev/null
   
-  if [ $? = 0 ]; then
+  if [ $? != 0 ]; then
     echo "Docker detected." 
-    echo "Downloading client."
-    get_client
-    echo "Done."
     echo "Building image."
     build_image
-    echo "Done."
-    echo "Running tus-spring-boot. Cheers!"
-    rm settings.xml
-    rm -rf $client_dir 
-    run_container
+    
+    if [ $? = 0 ]; then
+      echo "Done."
+      echo "Running tus-spring-boot. Cheers!"
+      rm settings.xml
+      rm -rf $client_dir 
+      run_container
+    else
+      echo "Error! Failed to build the image."
+      exit 1
+    fi
   else
-    echo "Docker not detected." 
-    echo "Downloading client."
-    get_client
-    echo "Done."
-    echo "Building client."
-  	build_client
-    echo "Done."
-    echo "Running tus-spring-boot. Enjoy!"
-		rm -rf $client_dir
-    run_app
-    rm -rf src/main/resources/public
+    echo "Docker not detected."
+    
+    if [ ! -d "src/main/resources/public" ]; then
+      if [ ! -f "$jar_path" ]; then
+        echo "Creating build for first-time run."
+        opt="--clean-all"
+      fi
+    fi
+
+    build_app $opt
+    
+    if [ $? = 0 ]; then
+      echo "Done."
+      echo "Running tus-spring-boot. Enjoy!"
+  		rm -rf $client_dir 
+      rm -rf src/main/resources/public
+      run_app
+    else
+      echo "Error! Failed to build the application."
+      exit 1
+    fi
   fi
 }
 
-main
+main $1
